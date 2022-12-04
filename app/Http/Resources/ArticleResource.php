d5b9523440a84695b2a3ffdf4f14c5a17d5120cf
Mohammed Hassan <medo.hassan0100@gmail.com>
2022-12-04 18:01:05 +0200

    article

--- app/Http/Resources/ArticleResource.php
+++ app/Http/Resources/ArticleResource.php
@@ -15,11 +15,12 @@ class ArticleResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'     => $this->id,
-            'title'  => $this->title,
-            'image'  => asset($this->image),
-            'banner' => asset($this->banner),
-            'desc'   => $this->desc,
+            'id'        => $this->id,
+            'title'     => $this->title,
+            'image'     => asset($this->image),
+            'banner'    => asset($this->banner),
+            'desc'      => $this->desc,
+            'date'      => $this->created_at->format('d/m/Y')
         ];
     }
 }
